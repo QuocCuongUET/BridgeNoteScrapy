26c725f970b6352d36b88c93351920075a2c7cad
CuongLH <cuonglh_58@vnu.edu.vn>
2018-11-05 10:20:39 +0700

    run first spider return data json

--- BridgeNoteScrapy/spiders/kemenkeu_spider.py
+++ BridgeNoteScrapy/spiders/kemenkeu_spider.py
@@ -2,13 +2,12 @@ import scrapy
 from datetime import datetime, timedelta
 
 class KemenkeuSpider(scrapy.Spider):
-	name = 'kemenkeu'
+    name = "kemenkeu"
 
     def start_requests(self):
-        return [scrapy.FormRequest(
-                    'http://www.fiskal.kemenkeu.go.id/dw-kurs-db.asp',
-                    formdata = {'strDate': '20181029', 'id': '10/29/2018'},
-                    callback = self.parse)]
+        return [scrapy.FormRequest( 'http://www.fiskal.kemenkeu.go.id/dw-kurs-db.asp',
+                                    formdata = {'strDate': '20181029', 'id': '10/29/2018'},
+                                    callback = self.parse)]
 
     def parse(self, response):
         table = response.css('div.table-responsive > table')
